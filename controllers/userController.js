52e8fe6612553d1ee53cc2e10babf6564deac135
hbd08092022 <112827086+RjForLife@users.noreply.github.com>
2023-09-12 01:57:53 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,10 @@ export const myProfile = (req, res, next) => {
 
 export const logout = (req, res, next) => {
     req.session.destroy((err) => {
-        if (err)  return next(err);
+        if (err){      
+            console.error('Error destroying session:', err);
+            return next(err);
+        }
         
         res.clearCookie('connect.sid')
         res.status(200).json({
