de2974182d7f5f386cd17f96053f522a81ca4d5e
hbd08092022 <112827086+RjForLife@users.noreply.github.com>
2023-09-20 00:28:53 +0530

    Update app.js
    changed front end url

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({
 }))
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://foodiehub-plum.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"]
 
 }))
